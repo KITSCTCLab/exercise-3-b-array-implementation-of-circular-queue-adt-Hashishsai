1aa5aac0b1f4d2fcb6d00b6127237e1d27254aa7
URK21CO3027 HASHISH SAI <110015215+Hashishsai@users.noreply.github.com>
2022-08-02 11:53:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,53 +1,94 @@
-class MyCircularQueue:
-    def __init__(self, size: int):
-        # Write code here
-
-    def enqueue(self, value: int) -> bool:
-        # Write code here
-
-    def dequeue(self) -> bool:
-        # Write code here
-
-    def get_front(self) -> int:
-        # Write code here
-
-    def get_rear(self):
-        # Write code here
-
-    def is_empty(self):
-        # Write code here
-
-    def is_full(self):
-        # Write code here
+import operator
+class Evaluate:
+  """This class validates and evaluate postfix expression.
+  Attributes:
+      top: An integer which denotes the index of the element at the top of the stack currently.
+      size_of_stack: An integer which represents the size of stack.
+      stack: A List which acts as a Stack.
+  """
+    # Write your code here
 
 
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-data = []
-for item in input().split(','):
-    item = item.strip()
-    if item == '-':
-        data.append([])
-    else:
-        data.append([int(item)])
-obj = MyCircularQueue(data[0][0])
-result = []
-for i in range(len(operations)):
-    if i == 0:
-        result.append(None)
-    elif operations[i] == "enqueue":
-        result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
-        result.append(obj.get_rear())
-    elif operations[i] == "get_front":
-        result.append(obj.get_front())
-    elif operations[i] == "dequeue":
-        result.append(obj.dequeue())
-    elif operations[i] == "is_full":
-        result.append(obj.is_full())
-    elif operations[i] == "is_empty":
-        result.append(obj.is_empty())
+  def __init__(self, size):
+    """Inits Evaluate with top, size_of_stack and stack.
+    Arguments:
+      size_of_stack: An integer to set the size of stack.
+    """
+    self.top = -1
+    self.size_of_stack = size
+    self.stack = []
 
-print(result)
+
+  def isEmpty(self):
+    """
+    Check whether the stack is empty.
+    Returns:
+      True if it is empty, else returns False.
+    """
+    return self.top == -1
+
+
+  def mypop(self):
+    """
+    Do pop operation if the stack is not empty.
+    Returns:
+      The data which is popped out if the stack is not empty.
+    """
+    if not self.isEmpty():
+        x = self.stack.pop()
+        self.top = self.top - 1
+        return x
+
+
+  def push(self, operand):
+    """
+    Push the operand to stack if the stack is not full.
+    Arguments:
+      operand: The operand to be pushed.
+    """
+    self.top += 1
+    self.stack.append(operand)
+
+
+  def validate_postfix_expression(self, expression):
+    """
+    Check whether the expression is a valid postfix expression.
+    Arguments:
+      expression: A String which represents the expression to be validated.
+    Returns:
+      True if the expression is valid, else returns False.
+    """
+    counter_digit = counter_operand = 0
+    for token in expression:
+        if token.isdigit():
+            counter_digit += 1
+        else:
+            counter_operand += 1
+    return counter_digit == counter_operand + 1
+
+
+  def evaluate_postfix_expression(self, expression):
+    """
+    Evaluate the postfix expression
+    Arguments:
+      expression: A String which represents the the expression to be evaluated
+    Returns:
+      The result of evaluated postfix expression.
+    """
+    ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    '*' : operator.mul,
+    '/' : operator.truediv,
+    '%' : operator.mod,
+    '^' : operator.xor,
+    }
+    for token in expression:
+        if token.isdigit():
+            self.push(int(token))
+        else:
+            operand2 = self.mypop()
+            operand1 = self.mypop()
+            result = ops[token](operand1, operand2)
+            self.push(int(result))
+    return self.stack[0]
